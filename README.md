278f0c014e23048a4907d48fe59c5d3b29d5fb44
CodeSandbox <support@codesandbox.io>
2023-08-20 04:11:38 +0000

    Sandbox snapshot commit

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# Docker Example
+# rainbow-grid
+A simple responsive rainbow grid animation using React and Anime.js.
 
-In this example, we show how you can use [Docker with CodeSandbox](https://codesandbox.io/post/introducing-docker-support-in-codesandbox). Check [the Dockerfile](./.codesandbox/Dockerfile) to see how we configure the container of this sandbox. You can simply edit it yourself and the container will rebuild. Make sure to fork the sandbox first by clicking "Fork" in the top left corner.
+##
 
-- Every new terminal will run inside this new container, open a new terminal with CTRL ~
-- We set up an example sandbox task that runs `whereis htop`. Typically this would be where you run `yarn run serve`. You can edit it in [tasks.json](./.codesandbox/tasks.json)
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import anime from 'animejs';
+import randomColor from 'randomcolor';
+
+import './App.css';
+
+function App() {
+  const [columns, setColumns] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [total, setTotal] = useState(1);
+  const [color, setColor] = useState('#ffffff');
+
+  function handleStagger({ target: { id: element } }) {
+    const randColor = randomColor();
+    setColor(randColor);
+
+    anime({
+      targets: '.grid-item',
+      backgroundColor: randColor,
+      delay: anime.stagger(50, {
+        grid: [columns, rows],
+        from: element
+      })
+    });
+  }
+
+  function setupGrid(hexCode) {
+    const columnsNum = Math.floor(document.body.clientWidth / 50);
+    const rowsNum = Math.floor(document.body.clientHeight / 50);
+
+    setColumns(columnsNum);
+    setRows(rowsNum);
+    setTotal(rowsNum * columnsNum);
+    setColor(hexCode || '#ffffff');
+
+    anime({
+      targets: '.grid-item',
+      backgroundColor: hexCode || '#ffffff',
+      duration: 0,
+      easing: 'linear'
+    });
+  }
+
+  useEffect(() => setupGrid('#ffffff'), []);
+  window.onresize = () => setupGrid(color);
+
+  return (
+    <>
+      <div id="grid">
+        {[...Array(total)].map((...args) => (
+          <div key={(args[1] + 1).toString()} className="grid-item" id={args[1]} onClick={event => handleStagger(event)}></div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App; // All bugs are not bugs. It's a feature.
